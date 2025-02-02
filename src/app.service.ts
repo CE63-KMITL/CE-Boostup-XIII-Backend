d804a60908fbbd05b02058b730dcd1f028a5f747
AzPepoze <65204787+AzPepoze@users.noreply.github.com>
2025-02-02 21:09:07 +0700

    Rename getHello method to Home_Page and update its implementation to return a localized message; comment out related test cases.

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-	getHello(): string {
-		return "Hello World!";
+	Home_Page(): string {
+		return "คุณไม่ควรมาอยู่ตรงนี้นะ";
 	}
 }
