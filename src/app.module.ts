2feaa4da9584819b04a38b0fc76f75a4cacceea8
Potsawee <potsaweee.p@gmail.com>
2025-02-09 16:08:32 +0700

    feat: connect app to PostgreSQL using TypeORM and set up Swagger documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,35 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ProblemModule } from './problem/problem.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { dotenvConfig } from './shared/configs/dotenv.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from './shared/configs/databaseconfig';
+import { GLOBAL_CONFIG } from './shared/constants/global-config.constant';
 
 @Module({
-	imports: [UserModule, ProblemModule],
-	controllers: [AppController],
-	providers: [AppService],
+   imports: [
+      UserModule,
+      ProblemModule,
+      ConfigModule.forRoot({
+         isGlobal: true,
+         validationSchema: dotenvConfig,
+      }),
+      TypeOrmModule.forRootAsync({
+         imports: [ConfigModule],
+         useFactory: (configService: ConfigService) => ({
+            ...databaseConfig,
+            autoLoadEntities: true,
+            synchronize: configService.get<boolean>(
+               GLOBAL_CONFIG.IS_DEVELOPMENT,
+            ),
+         }),
+         inject: [ConfigService],
+      }),
+   ],
+   controllers: [AppController],
+   providers: [AppService],
 })
 export class AppModule {}
