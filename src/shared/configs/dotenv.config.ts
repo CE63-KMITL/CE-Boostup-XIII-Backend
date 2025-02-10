e9180d326dfb258ec6a7947c365a8f30be026213
Potsawee <potsaweee.p@gmail.com>
2025-02-10 19:32:30 +0700

    update dotnevcofig file

--- src/shared/configs/dotenv.config.ts
+++ src/shared/configs/dotenv.config.ts
@@ -1,10 +1,11 @@
 import * as Joi from 'joi';
 
 export const dotenvConfig = Joi.object({
-  PORT: Joi.number().required(),
-  DB_HOST: Joi.string().required(),
-  DB_PORT: Joi.number().required(),
-  DB_USERNAME: Joi.string().required(),
-  DB_PASSWORD: Joi.string().required(),
-  DB_NAME: Joi.string().required(),
+   PORT: Joi.number().required(),
+   DB_HOST: Joi.string().required(),
+   DB_PORT: Joi.number().required(),
+   DB_USERNAME: Joi.string().required(),
+   DB_PASSWORD: Joi.string().required(),
+   DB_NAME: Joi.string().required(),
+   IS_DEVELOPMENT: Joi.boolean().required(),
 });
