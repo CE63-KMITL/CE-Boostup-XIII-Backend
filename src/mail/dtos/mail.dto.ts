c67441ea9a743d7cc58c2bf70e5bd0e2b4f8679e
Potsawee <potsaweee.p@gmail.com>
2025-02-22 20:32:24 +0700

    feat: add swagger in mail service

--- src/mail/dtos/mail.dto.ts
+++ src/mail/dtos/mail.dto.ts
@@ -1,19 +1,40 @@
-import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class sendMailDto {
-   @IsEmail({}, { each: true })
-   @IsNotEmpty()
-   recipients: string[];
+  @IsEmail({}, { each: true })
+  @IsNotEmpty()
+  @ApiProperty({
+    type: [String],
+    example: ["example@gmail.com"],
+    description: "Email recipients",
+  })
+  recipients: string[];
 
-   @IsString()
-   @IsNotEmpty()
-   subject: string;
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: "Subject",
+    description: "Email subject",
+    type: String,
+  })
+  subject: string;
 
-   @IsString()
-   @IsNotEmpty()
-   html: string;
+  @IsString()
+  @IsNotEmpty()
+  @ApiPropertyOptional({
+    example: "<h1>HTML Content</h1>",
+    description: "Email HTML content",
+    type: String,
+  })
+  html: string;
 
-   @IsOptional()
-   @IsString()
-   text?: string;
+  @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({
+    example: "Text Content",
+    description: "Email text content",
+    type: String,
+  })
+  text?: string;
 }
