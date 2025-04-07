5a8599ca4c0a973c91077a5d4f6c3718067b6555
AzPepoze <65204787+AzPepoze@users.noreply.github.com>
2025-04-07 17:48:30 +0700

    refactor: update RunCodePostDto to make input and timeout optional

--- src/run_code/dtos/run_code-post.dto.ts
+++ src/run_code/dtos/run_code-post.dto.ts
@@ -1,19 +1,18 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsNumber, IsOptional, IsString } from "class-validator";
 
 export class RunCodePostDto {
 	@ApiProperty({ description: "Input for the code execution", example: "1 2" })
 	@IsString()
-	@IsNotEmpty()
+	@IsOptional()
 	input: string;
 
 	@ApiProperty({ description: "The code to be executed", example: "print(sum(map(int, input().split())))" })
 	@IsString()
-	@IsNotEmpty()
 	code: string;
 
 	@ApiProperty({ description: "Timeout for the code execution in milliseconds", example: 1000 })
 	@IsNumber()
-	@IsNotEmpty()
+	@IsOptional()
 	timeout: number;
 }
