01986f63340ab112f9d61017d3740bb8c5d0d009
AzPepoze <65204787+AzPepoze@users.noreply.github.com>
2025-04-07 12:25:24 +0700

    refactor: reorganize user score management structure and remove unused DTOs

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,43 +1,36 @@
 import { IsEmail, IsNumber } from "class-validator";
-import {
-  Entity,
-  Column,
-  PrimaryGeneratedColumn,
-  CreateDateColumn,
-  UpdateDateColumn,
-  OneToMany,
-} from "typeorm";
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Role } from "../shared/enum/role.enum";
-import { ScoreLog } from "./score-log.entity";
+import { ScoreLog } from "./score/score-log.entity";
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn("uuid")
-  id: string;
+	@PrimaryGeneratedColumn("uuid")
+	id: string;
 
-  @Column({ nullable: false, unique: true })
-  name: string;
+	@Column({ nullable: false, unique: true })
+	name: string;
 
-  @Column({ nullable: false })
-  password: string;
+	@Column({ nullable: false })
+	password: string;
 
-  @Column({ nullable: false, unique: true })
-  @IsEmail()
-  email: string;
+	@Column({ nullable: false, unique: true })
+	@IsEmail()
+	email: string;
 
-  @Column({ nullable: false, default: Role.MEMBER, enum: Role, type: "enum" })
-  role: Role;
+	@Column({ nullable: false, default: Role.MEMBER, enum: Role, type: "enum" })
+	role: Role;
 
-  @Column({ nullable: false, default: 0 })
-  @IsNumber()
-  score: number;
+	@Column({ nullable: false, default: 0 })
+	@IsNumber()
+	score: number;
 
-  @CreateDateColumn({ type: "timestamp", nullable: false })
-  createdAt: Date;
+	@CreateDateColumn({ type: "timestamp", nullable: false })
+	createdAt: Date;
 
-  @UpdateDateColumn({ type: "timestamp", nullable: false })
-  updatedAt: Date;
+	@UpdateDateColumn({ type: "timestamp", nullable: false })
+	updatedAt: Date;
 
-  @OneToMany(() => ScoreLog, (scoreLog) => scoreLog.user)
-  scoreLogs: ScoreLog[];
+	@OneToMany(() => ScoreLog, (scoreLog) => scoreLog.user)
+	scoreLogs: ScoreLog[];
 }
