57b9298d5f2cded697480e633ed76524681eca40
DEKDDD <supawe2548@gmail.com>
2025-05-08 17:10:33 +0700

    debug register

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -1,8 +1,25 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsNotEmpty, IsEmail, IsEnum, IsOptional } from 'class-validator';
+import {
+	IsEmail,
+	IsEnum,
+	IsNotEmpty,
+	IsOptional,
+	IsString,
+	Matches,
+} from 'class-validator';
 import { House } from 'src/shared/enum/house.enum';
+import { AvailableRole, Role } from 'src/shared/enum/role.enum';
 
 export class RegisterUserDto {
+	@IsString()
+	@IsOptional()
+	@ApiPropertyOptional({
+		example: 'john_doe',
+		description: 'name',
+		type: String,
+	})
+	name?: string;
+
 	@IsEmail()
 	@IsNotEmpty()
 	@ApiProperty({
@@ -11,6 +28,15 @@ export class RegisterUserDto {
 		type: String,
 	})
 	email: string;
+
+	@IsOptional()
+	@IsString()
+	@ApiPropertyOptional({
+		example: 'P@ssword1234',
+		description: 'Password',
+		type: String,
+	})
+	password?: string;
 
 	@IsOptional()
 	@IsEnum(House, {
@@ -23,4 +49,26 @@ export class RegisterUserDto {
 		type: String,
 	})
 	house?: House;
+
+	@IsOptional()
+	@IsEnum(AvailableRole, {
+		message: `Role must be a valid enum value: ${Role.DEV} or ${Role.STAFF} or ${Role.MEMBER}`,
+	})
+	@ApiPropertyOptional({
+		example: Role.MEMBER,
+		description: 'User role',
+		enum: AvailableRole,
+		type: String,
+		default: Role.MEMBER,
+	})
+	role?: Role;
+
+	@IsOptional()
+	@Matches(/^\d{8}$/, { message: 'student id should be 8-digit number ' })
+	@ApiPropertyOptional({
+		example: '67011501',
+		description: 'student id',
+		type: String,
+	})
+	studentId?: string;
 }
