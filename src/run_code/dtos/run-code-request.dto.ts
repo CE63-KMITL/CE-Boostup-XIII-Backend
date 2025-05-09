1929e4df2de8281b00f5c2a1c0b6dc29ec69b1ea
AzPepoze <pladumguy@gmail.com>
2025-05-09 14:31:22 +0700

    ✨ Enhances problem creation and code execution
    Adds functionality to define allowed headers and functions for problems.
    Introduces a new endpoint to run code against multiple test cases, facilitating more comprehensive testing.

--- src/run_code/dtos/run-code-request.dto.ts
+++ src/run_code/dtos/run-code-request.dto.ts
@@ -1,6 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Expose } from 'class-transformer';
-import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsArray, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class RunCodeRequestDto {
 	@ApiProperty({
@@ -10,6 +9,32 @@ export class RunCodeRequestDto {
 	@IsString()
 	@IsOptional()
 	input: string;
+
+	@ApiProperty({
+		description: 'The code to be executed',
+		example: 'print(sum(map(int, input().split())))',
+	})
+	@IsString()
+	code: string;
+
+	@ApiProperty({
+		description: 'Timeout for the code execution in milliseconds',
+		example: 1000,
+	})
+	@IsNumber()
+	@IsOptional()
+	timeout: number;
+}
+
+export class RunCodeTestCasesRequestDto {
+	@ApiProperty({
+		description: 'Input for the code execution',
+		example: ['1 2', '3 4'],
+	})
+	@IsString({ each: true })
+	@IsOptional()
+	@IsArray()
+	inputs: string[];
 
 	@ApiProperty({
 		description: 'The code to be executed',
--- src/run_code/run-code.controller.ts
+++ src/run_code/run-code.controller.ts
@@ -1,6 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { RunCodeRequestDto } from './dtos/run-code-request.dto';
+import {
+	RunCodeRequestDto,
+	RunCodeTestCasesRequestDto,
+} from './dtos/run-code-request.dto';
 import { RunCodeService } from './run-code.service';
 import { RunCodeResponseDto } from './dtos/run-code-response.dto';
 import { AllowRole } from 'src/auth/decorators/auth.decorator';
@@ -20,10 +23,29 @@ export class RunCodeController {
 	})
 	@AllowRole(Role.MEMBER)
 	runCode(@Body() body: RunCodeRequestDto): Promise<RunCodeResponseDto> {
-		return this.runCodeService.runCode(
-			body.input,
-			body.code,
-			100,
-		);
+		return this.runCodeService.runCode(body.input, body.code, 100);
+	}
+
+	@Post('/test-cases')
+	@ApiOperation({ summary: 'Run code with input' })
+	@ApiResponse({
+		status: 200,
+		description: 'Code executed successfully',
+		type: RunCodeResponseDto,
+		isArray: true,
+	})
+	@AllowRole(Role.STAFF)
+	runCodeTestCases(
+		@Body() body: RunCodeTestCasesRequestDto,
+	): Promise<RunCodeResponseDto[]> {
+		const result = [];
+
+		for (const input of body.inputs) {
+			result.push(
+				this.runCodeService.runCode(input, body.code, body.timeout),
+			);
+		}
+
+		return Promise.all(result);
 	}
 }
