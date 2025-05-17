2592039cd2b5f8823c50016cb8756c9871202585
Potsawee <potsaweee.p@gmail.com>
2025-05-17 23:19:02 +0700

    feat: creat update problem dto

--- src/problem/dtos/problem-update.dto.ts
+++ src/problem/dtos/problem-update.dto.ts
@@ -1,4 +1,106 @@
-import { CreateProblemDto } from './problem-create.dto';
-import { OmitType } from '@nestjs/swagger';
+import {
+	IsArray,
+	IsEnum,
+	IsIn,
+	IsNumber,
+	IsOptional,
+	IsString,
+	ValidateNested,
+} from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { ProblemAllowMode } from '../enums/problem-allow-mode.enum';
+import { TestCase, CreateTestCase } from '../test_case/test-case.object';
+import { SCORE_VALUES, ScoreValue } from '../types/score-value.type';
 
-export class UpdateProblemDto extends CreateProblemDto {}
+export class UpdateProblemDto {
+	@ApiPropertyOptional({ example: 'Sample Problem Title' })
+	@IsOptional()
+	@IsString()
+	title: string;
+
+	@ApiPropertyOptional({ example: 'Sample problem description' })
+	@IsOptional()
+	@IsString()
+	description?: string;
+
+	@ApiPropertyOptional({
+		description: 'Time limit in milliseconds',
+		example: 100,
+		default: 100,
+	})
+	@IsNumber()
+	@IsOptional()
+	timeLimit?: number = 100;
+
+	@ApiPropertyOptional({
+		example: ProblemAllowMode.DISALLOWED,
+		enum: ProblemAllowMode,
+	})
+	@IsEnum(ProblemAllowMode)
+	@IsOptional()
+	headerMode?: ProblemAllowMode;
+
+	@ApiPropertyOptional({
+		example: ['stdio.h'],
+	})
+	@IsOptional()
+	@IsArray()
+	@IsString({ each: true })
+	headers?: string[];
+
+	@ApiPropertyOptional({
+		example: ProblemAllowMode.DISALLOWED,
+		enum: ProblemAllowMode,
+	})
+	@IsOptional()
+	@IsEnum(ProblemAllowMode)
+	functionMode?: ProblemAllowMode;
+
+	@ApiPropertyOptional({
+		example: ['for'],
+	})
+	@IsOptional()
+	@IsArray()
+	@IsString({ each: true })
+	functions?: string[];
+
+	@ApiPropertyOptional({
+		example: '#include <stdio.h>\n\nint main() {\n\tprintf("Hello, World!");\n\treturn 0;\n}',
+	})
+	@IsOptional()
+	@IsString()
+	defaultCode?: string;
+
+	@ApiPropertyOptional({
+		example: '#include <stdio.h>\n\nint main() {\n\tprintf("Hello, World!");\n\treturn 0;\n}',
+	})
+	@IsString()
+	@IsOptional()
+	solutionCode?: string;
+
+	@ApiPropertyOptional({
+		example: 3,
+		description: 'Difficulty level (0.5 to 5)',
+	})
+	@IsNumber()
+	@IsOptional()
+	@Type(() => Number)
+	@IsIn(SCORE_VALUES)
+	difficulty?: ScoreValue;
+
+	@ApiPropertyOptional({ example: ['Basic I/O', 'If - else'] })
+	@IsOptional()
+	@IsArray()
+	tags?: string[];
+
+	@ApiPropertyOptional({
+		type: [TestCase],
+		description: 'Test cases for the problem',
+	})
+	@ValidateNested({ each: true })
+	@IsArray()
+	@Type(() => CreateTestCase)
+	@IsOptional()
+	testCases?: CreateTestCase[];
+}
